d681cc65a5b4238a481d7fc95625ccbde354852c
Mark Leybourne <mark.leybourne@sage.com>
2023-03-03 10:50:59 +0000

    WIP

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { Construct } from "constructs";
 import * as cdktf from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { SnowflakeProvider } from "@cdktf/provider-snowflake/lib/provider";
 
 class MyStack extends cdktf.TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -9,6 +10,11 @@ class MyStack extends cdktf.TerraformStack {
 
     new AwsProvider(this, "AWS", {
       region: "us-west-1",
+    });
+
+    new SnowflakeProvider(this, "Snowflake", {
+      account: "myaccount",
+      username: "myusername"
     });
 
     const ec2Instance = new Instance(this, "compute", {
