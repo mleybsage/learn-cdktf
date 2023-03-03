45bd465075f4aa05bd5c833ae0192f3020164166
Mark Leybourne <mark.leybourne@sage.com>
2023-03-03 10:44:18 +0000

    Initial commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { Construct } from "constructs";
+import * as cdktf from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
+
+class MyStack extends cdktf.TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new AwsProvider(this, "AWS", {
+      region: "us-west-1",
+    });
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: "ami-01456a894f71116f2",
+      instanceType: "t2.micro",
+    });
+
+    new cdktf.TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
+  }
+}
+
+const app = new cdktf.App();
+
+new MyStack(app, "aws_instance");
+
+app.synth();
